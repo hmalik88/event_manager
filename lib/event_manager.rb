ea7469be83843a6157ce09258423ba57833f3700
hmalik88 <hbmalik88@gmail.com>
2020-05-07 01:35:00 -0400

    some fixes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,17 +6,18 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
-def legislators_by_zipcode(zipcode)
+def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
   begin
-    civic_info.representative_info_by_address(address: zipcode, levels: 'country', roles: ['legislatorUpperBody', 'legislatorLowerBody'])
+    civic_info.representative_info_by_address(address: zip, levels: 'country', roles: ['legislatorUpperBody', 'legislatorLowerBody']).officials
   rescue
     "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
   end
 end
 
 def save_thank_you_letter(id, form_letter)
+  Dir.mkdir "output" unless Dir.exists? "output"
   filename = "output/thanks_#{id}.html"
   File.open(filename, "w") do |file|
     file.puts(form_letter)
@@ -34,6 +35,8 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
+
   form_letter = erb_template.result(binding)
+
   save_thank_you_letter(id, form_letter)
 end
